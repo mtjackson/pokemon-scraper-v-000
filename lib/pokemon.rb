04f39d38097e411c771eba7c9348f5d977f1d029
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 00:47:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(id, db)
+    self.all.each do |pokemon|
+      if pokemon.id == id
+        self.save(pokemon.name, pokemon.type, db)
+      end
+    end
+  end
 end
