192a264aa0cc68d1e0c135480ae39c3db3619c13
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 00:57:50 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
 
   def self.find(id, db)
     x = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    self.new(x.name, x.type, db)
+    self.new(x[1], x[2], db)
   #  self.all.each do |pokemon|
   #    if pokemon.id == id
   #      self.save(pokemon.name, pokemon.type, db)
