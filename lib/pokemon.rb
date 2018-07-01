556a68c0ba477534959bd810fcc0b08378f3ed17
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 00:53:08 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.exeucte
+    db.exeucte("SELECT")
     self.all.each do |pokemon|
       if pokemon.id == id
         self.save(pokemon.name, pokemon.type, db)
